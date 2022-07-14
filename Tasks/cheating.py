3c8470ea23fb2426a6d8edb4b04bfb1ca17c82ea
Vuker <vakulenko11@gmail.com>
2022-07-14 20:17:02 +0300

    Check IDE Git.

---
+++ Tasks/cheating.py
@@ -0,0 +1,14 @@
+def remov_nb(n):
+    all_variants = [x for x in range(1, n + 1)]
+    sum_all = sum(all_variants)
+    poss_pairs = []
+    for num in all_variants:
+        check_form = (sum_all - num) / (num + 1)
+        if check_form.is_integer() and check_form in all_variants:
+            poss_pairs.append((int(num), int(check_form)))
+    return poss_pairs
+
+
+if __name__ == '__main__':
+    user_input = int(input())
+    print(remov_nb(user_input))
